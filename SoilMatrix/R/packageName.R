baa35f29d7b39a92f1b0d963b0755fc5a045ac9f
Emma <emhauser@ku.edu>
2018-04-17 11:46:24 -0500

    adding package name .R file

---
+++ SoilMatrix/R/packageName.R
@@ -0,0 +1,12 @@
+#' SoilMatrix: A package to organize functions needed for soil biogeochemical analyses.
+#
+#' SoilMatrix contains functions:
+#' interpolate
+#'
+#' @section SoilMatrix functions:
+#' functions ...
+#'
+#' @docType package
+#' @name SoilMatrix
+NULL
+
